1120bec6e4e05908120a7733ddf69c99196928b9
P Benny Joseph <jbenny37@gmail.com>
2020-11-18 22:04:45 +0530

    added sockets

--- server/src/app.js
+++ server/src/app.js
@@ -1,58 +1,68 @@
-const { assert } = require("console");
-const express = require("express");
-const path = require("path");
-
-const app = express();
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
-
+const express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server, {
+  cors:true,
+  origins:["http://127.0.0.1:5500"],
+ });  
 let currentRoom = 0;
 
-app.use(express.static("./public"));
+// app.use(express.static('../client'));
+// app.get('/hex', (req, res) => {
+//   res.sendFile("../client/Hex.html");
+// });
 
 io.on("connection", (socket) => {
   socket.on("createOrJoinGame", (data) => {
-    var room = io.nsps[data.nsps].adapter.rooms[currentRoom];
-    if (room && room.length < 2) {
+    var room = io._nsps.get('/').adapter.rooms.get(currentRoom);
+    // console.log(io._nsps.get('/').adapter);
+    if(room) console.log("Size: " + room.size);
+    if (room && room.size < 2) {
       socket.join(currentRoom);
       socket.emit("player2", {
-        msg: "Game has begun. Waiting for opponent",
+        msg: "Game has begun. Waiting for opponent to play",
+        player: 1,
         id: currentRoom,
       });
+      socket.broadcast.to(currentRoom).emit('startGame');
+      socket.emit('startGame');
       ++currentRoom;
+      console.log("Joining room");
     } else {
-      ++currentRoom;
       socket.join(currentRoom);
       socket.emit("newGame", {
+        msg: "Waiting for other players to join.",
+        player: 0,
         id: currentRoom,
       });
+      console.log('Creating new game');
     }
-    // console.log('Creating new game');
+    
   });
 
-  socket.on("joinGame", function (data) {
-    var room = io.nsps["/"].adapter.rooms[data.room];
-    if (room && room.length <= 3) {
-      socket.join(data.room);
-      socket.emit("player2", {
-        name: data.name,
-        room: data.room,
-        id: room.length - 1,
-      });
+  // socket.on("joinGame", function (data) {
+  //   var room = io.nsps["/"].adapter.rooms[data.room];
+  //   if (room && room.length <= 3) {
+  //     socket.join(data.room);
+  //     socket.emit("player2", {
+  //       name: data.name,
+  //       room: data.room,
+  //       id: room.length - 1,
+  //     });
 
-      if (room.length === 4) {
-        var color = CARDS[Math.floor(Math.random() * CARDS.length)];
-        var digit = digits[Math.floor(Math.random() * 19)];
-        var card = color + digit;
-        socket.nsp.to(data.room).emit("player1", { card });
-      }
-    } else {
-      socket.emit("err", {
-        message: "Sorry, The room is full!",
-      });
-    }
-    // console.log('joining new game');
-  });
+  //     if (room.length === 4) {
+  //       var color = CARDS[Math.floor(Math.random() * CARDS.length)];
+  //       var digit = digits[Math.floor(Math.random() * 19)];
+  //       var card = color + digit;
+  //       socket.nsp.to(data.room).emit("player1", { card });
+  //     }
+  //   } else {
+  //     socket.emit("err", {
+  //       message: "Sorry, The room is full!",
+  //     });
+  //   }
+  //   // console.log('joining new game');
+  // });
 
   socket.on("playTurn", (data) => {
     socket.broadcast.to(data.room).emit("turnPlayed", data);
@@ -63,4 +73,5 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(process.env.PORT || 5000);
+server.listen(process.env.PORT || 3000);
+console.log("listenting on port 3000");
