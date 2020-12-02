5398247c179cf950ab3bec4e79699872bfa52719
P Benny Joseph <jbenny37@gmail.com>
2020-12-02 07:30:51 +0530

    fix APIKEY leak

--- client/js/sockets.js
+++
@@ -1,8 +0,0 @@
-const socket = io.connect('http://localhost:3000');
-
-socket.on('player2', (data) => {});
-socket.on('isDisconnected', (data) => {});
-
-socket.emit('createorJoinGame', {
-  nsps: '10',
-});
